eadb61c34acb2dd928e0ddf98a9d7dc99d81ed9a
RulevAV <rulandrei99@mail.ru>
2022-02-21 00:35:42 +0700

    Login & Cookies

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import morgan from "morgan";
-
 import authRoutes from './routes/auth';
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 import trim from "./middleware/trim"
 
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim);
+app.use(cookieParser())
 
 app.get('/', (req, res) => res.send("Hello world"))
 app.use('/api/auth', authRoutes)
